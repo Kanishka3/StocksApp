594ed82172e508700db120a87bb37811bc4ede48
Kanishka Chaudhry <kanishka.chaudhry@grofers.com>
2024-10-31 22:50:24 +0530

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Kanishka Chaudhry on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
